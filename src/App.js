f7452b1eea9ebfc9d70b9389eb29af507eb17418
jjggu97 <jjggu@naver.com>
2024-06-13 21:37:58 +0900

    06/13

--- src/App.js
+++ src/App.js
@@ -1,40 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-function Header(){
+function Header(props){
   return(
       <header>
-      <h1><a href='/'>top menu</a></h1>
+      <h1><a href='/'>{props.title}</a></h1>
       </header>
   )
 }
 
-function Nav(){
+function Nav(props){
+  const lis = []
+  for(let i=0; i>props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read'+t.id}>{t.title}</a></li>);
+  }
   return(
   <nav>
     <ol>
-      <li><a href='/read/1'>html</a></li>
-      <li><a href='/read/2'>css</a></li>
-      <li><a href='/read/3'>js</a></li>
+      {lis} 
     </ol>
   </nav>
   )
 }
 
-function Article(){
+function Article(props){
   return(
     <article>
-        <h2>Welcome</h2>
-        Hello, World!
+        <h2>{props.body}</h2>
     </article>
   )
 }
 
 function App() {
+  const topics = [
+    {id:1, title:'html', body:'html is...'},
+    {id:2, title:'css', body:'css is...'},
+    {id:3, title:'javascript', body:'javascript is...'}
+  ]
+
   return (
     <div>
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title="jjggu"></Header>
+      <Nav topics={topics}></Nav>
+      <Article body="whole new world"></Article>
 
     </div>
   );
