a5c3db0c7ba63527a0d1ee356fade06292d1b801
jjggu97 <jjggu@naver.com>
2024-06-13 21:18:24 +0900

    06/13

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 function Header(){
   return(
-    <div>
       <header>
       <h1><a href='/'>top menu</a></h1>
       </header>
-    </div>
+  )
+}
+
+function Nav(){
+  return(
+  <nav>
+    <ol>
+      <li><a href='/read/1'>html</a></li>
+      <li><a href='/read/2'>css</a></li>
+      <li><a href='/read/3'>js</a></li>
+    </ol>
+  </nav>
+  )
+}
+
+function Article(){
+  return(
+    <article>
+        <h2>Welcome</h2>
+        Hello, World!
+    </article>
   )
 }
 
@@ -14,20 +33,9 @@ function App() {
   return (
     <div>
       <Header></Header>
-      <Header></Header>
-      <Header></Header>
-      <nav>
-        <ol>
-          <li><a href='/read/1'>html</a></li>
-          <li><a href='/read/2'>css</a></li>
-          <li><a href='/read/3'>js</a></li>
-        </ol>
-      </nav>
+      <Nav></Nav>
+      <Article></Article>
 
-      <article>
-        <h2>Welcome</h2>
-        Hello, World!
-      </article>
     </div>
   );
 }
