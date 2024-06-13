721a3094f766a658e55b965bc30c9647e8399fb3
jjggu97 <jjggu@naver.com>
2024-06-13 18:54:31 +0900

    06/13

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+function Header(){
+  return(
+    <div>
+      <header>
+      <h1><a href='/'>top menu</a></h1>
+      </header>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      React Test Message
+    <div>
+      <Header></Header>
+      <Header></Header>
+      <Header></Header>
+      <nav>
+        <ol>
+          <li><a href='/read/1'>html</a></li>
+          <li><a href='/read/2'>css</a></li>
+          <li><a href='/read/3'>js</a></li>
+        </ol>
+      </nav>
+
+      <article>
+        <h2>Welcome</h2>
+        Hello, World!
+      </article>
     </div>
   );
 }
